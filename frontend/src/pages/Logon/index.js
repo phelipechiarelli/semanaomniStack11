c55a19d8f46a3432c2c97320c5ddb90dc9571d7f
Phelipe Chiarelli <phelipechiarelli@gmail.com>
2021-04-27 22:46:32 -0300

    .

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
-import logoImg from '../../assets/logo.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function Logon(){
     return(
@@ -10,7 +10,7 @@ export default function Logon(){
             <section className="form">
             <img src={logoImg} alt="Be the hero" />
             <form>
-                <h1>Logon</h1>
+                <h1>logon</h1>
                 <input placeholder="Sua ID"/>
                 <button className="button" type="submit">Entrar</button><br></br>
                 <a href="/register">
@@ -20,7 +20,7 @@ export default function Logon(){
             </form>
             </section>
 
-            <img src={heroesImg} alt="Heroes"/>
+            <img className="heroesImg" src={heroesImg} alt="Heroes"/>
         </div>
 
     )
